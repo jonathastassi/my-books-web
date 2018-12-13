f85d028c347cbe36497f5aeac52e82c6eba3c52f
Jonathas <jonathastassi@hotmail.com>
2018-12-13 00:28:45 -0200

    ajuste no login

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -2,9 +2,9 @@
   <div>
 	  <h5>Login</h5>
     <form @submit.prevent>
-      <label>E-mail</label>
+      <label for="email">E-mail</label>
       <input type="email" name="email" id="email" v-model="loginForm.email" v-on:keyup.enter="$event.target.nextElementSibling.focus()" autofocus placeholder="Digite o seu e-mail...">
-      <label>Senha</label>
+      <label for="password">Senha</label>
       <input type="password" name="password" id="password" v-model="loginForm.password" v-on:keyup.enter="$event.target.nextElementSibling.focus()"  placeholder="Digite a sua senha...">
       <button class="btn waves-effect waves-light green lighten-4 btn-flat" @click="login">Entrar</button>
       <!-- <router-link to="/signup">Criar uma conta</router-link> -->
